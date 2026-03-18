3ab1a7df477e25954c41b44d33f17716f7722c51
TringaliD <tringalidavide362@gmail.com>
2026-03-18 18:11:07 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// Inizializza la mappa
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19
+}).addTo(map);
+
+const uploadBtn = document.getElementById("uploadBtn");
+const photoInput = document.getElementById("photoInput");
+
+uploadBtn.onclick = () => photoInput.click();
+
+photoInput.onchange = async (e) => {
+  const file = e.target.files[0];
+  if (!file) return;
+
+  navigator.geolocation.getCurrentPosition(pos => {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = reader.result;
+
+      const marker = L.marker([lat, lng]).addTo(map);
+
+      marker.bindPopup(`
+        <div style="text-align:center">
+          <h3 style="margin:0 0 8px;color:#4ECCA3;font-size:16px;">Orbit</h3>
+          <img src="${img}" style="width:160px;border-radius:10px;">
+        </div>
+      `);
+    };
+
+    reader.readAsDataURL(file);
+  });
+};
